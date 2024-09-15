90b8b4c4b32f94bf7a55c99244fb83933149024c
MrCode <114862006+azhan0058@users.noreply.github.com>
2024-09-15 16:49:07 +0530

    Update cron.js

--- backend/corn/cron.js
+++ backend/corn/cron.js
@@ -1,7 +1,7 @@
 import cron from "cron";
 import https from "https";
 
-const URL = "http://localhost:5000";
+const URL = "https://mediaapp-f5qe.onrender.com";
 
 const job = new cron.CronJob("*/14 * * * *", function () {
 	https
